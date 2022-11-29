93c066629cb29653215b1c4c7943af8e8f6aca4b
Moks <daefin98@gmail.com>
2022-11-29 16:01:50 +0200

    Add image spin

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,10 +1,96 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import {
+  View,
+  Image,
+  StyleSheet,
+  ImageURISource,
+  Animated,
+  Button,
+} from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
 
-export default function Main() {
+interface Props {
+  imageSource?: ImageURISource;
+}
+
+export default function Main({ imageSource }: Props) {
+  const [dimensions, setDimensions] = useState<{ w: number; h: number } | null>(
+    null,
+  );
+
+  // Load image dimensions
+  useEffect(() => {
+    if (!dimensions && imageSource?.uri) {
+      Image.getSize(
+        imageSource.uri,
+        (w, h) => {
+          setDimensions({ w, h });
+        },
+        () => {
+          // TODO: Error
+        },
+      );
+    }
+  }, [dimensions, setDimensions, imageSource?.uri]);
+
+  const rotation = useRef(new Animated.Value(0)).current;
+  const [rotationTarget, setRotationTarget] = useState(0);
+
+  // Apply image spin animation
+  useEffect(() => {
+    Animated.timing(rotation, {
+      toValue: rotationTarget,
+      duration: 300,
+      easing: (x) => 1 - (1 - x) ** 3,
+      useNativeDriver: true,
+    }).start(() => {
+      if (rotationTarget >= 1) {
+        rotation.setValue(rotationTarget % 1);
+        setRotationTarget((rotTarget) => rotTarget % 1);
+      }
+    });
+  }, [rotation, rotationTarget]);
+
   return (
-    <View>
-      <Text>Main</Text>
+    <View style={styles.container}>
+      <Animated.Image
+        source={imageSource}
+        style={[
+          { width: dimensions?.w, height: dimensions?.h },
+          {
+            transform: [
+              {
+                rotate: rotation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: ['0deg', '360deg'],
+                }),
+              },
+            ],
+          },
+        ]}
+        resizeMode="contain"
+      />
+      <View style={styles.buttonRow}>
+        <Button
+          title="rotate"
+          onPress={() => {
+            setRotationTarget((rotTarget) => rotTarget + 0.25);
+          }}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    backgroundColor: 'black',
+    flex: 1,
+  },
+  buttonRow: {
+    width: '100%',
+    minHeight: 80,
+    backgroundColor: 'yellow',
+    flexDirection: 'row',
+  },
+});
